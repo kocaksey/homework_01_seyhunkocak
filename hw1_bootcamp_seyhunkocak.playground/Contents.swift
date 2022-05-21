3d4ab1381ebe81e81819c1d7b77a1acd315c970c
Seyhun Koçak <seyhunkocak11@gmail.com>
2022-05-21 14:32:03 +0300

    sets added

--- hw1_bootcamp_seyhunkocak.playground/Contents.swift
+++ hw1_bootcamp_seyhunkocak.playground/Contents.swift
@@ -28,3 +28,15 @@ pricesOfGoods.values
    //Sorting existent dictionary more to less and printing
 let pricesOfGoodsMoreToLess = pricesOfGoods.sorted{$0.value > $1.value}
 print(pricesOfGoodsMoreToLess)
+
+//Sets
+
+//In a Tango dance course, dancing shoes will be purchase for dancers, and size of feet of dancers will be given below.
+var sizeOfFeetofDancers : [Int] = [36,38,36,41,37,43,43,37,41,40,44,43,45,41,36,37]
+//Specifying different size numbers of shoes to order and printing
+var numberOfTypesOfShoes = Set(sizeOfFeetofDancers)
+
+//Sorting types of shoe numbers less to more and printing
+let sortedNumberOfTypesOfShoes = numberOfTypesOfShoes.sorted()
+print(sortedNumberOfTypesOfShoes)
+
